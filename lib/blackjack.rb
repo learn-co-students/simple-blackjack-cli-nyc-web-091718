670435fcdabafd07998aee0e88d00e961d2ec0f3
Casey Zimmer <czimmer@ramapo.edu>
2018-09-13 01:33:30 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,56 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  temp =  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  total = 0
+  total += deal_card
+  total += deal_card
+  display_card_total(total)
+  return total
 end
 
-def hit?
-  # code hit? here
+def hit?(total)
+  loop do
+  prompt_user
+  input = get_user_input
+  
+  if(input == 's')
+  break
+  elsif(input =='h')
+    total += deal_card
+    break
+  else
+    invalid_command
+    prompt_user
+  end
+  end
+  return total
+  
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +58,26 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  
+  total = 0 
+  welcome
+  total = initial_round
+
+  until total > 21
+    
+    temp_amount = hit?(total)
+    if temp_amount == total
+    end_game(total)
+    break
+    else
+      total += temp_amount - total
+      if total > 21
+      break
+      end
+    end
+  end
+display_card_total(total)
+end_game(total)
+  
 end
     
